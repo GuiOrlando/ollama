de1861b667c10e0ce8d397a3d63310028a640e9f
Lei Jitang <leijitang@outlook.com>
2024-06-07 20:46:13 +0800

    llm/server.go: Fix ollama ps show 100%GPU even use CPU as runner
    Even the machine has GPU, but it still will fall back to
    use CPU as runner. But the `estimatedVRAM` doesn't clear,
    this make `ollama ps` show the model use `100% GPU` even
    it fully use CPU.
    ```
    llama3:latest   365c0bd3c000    5.4 GB  100% GPU        4 minutes from now
    ```
    But from the server log, we can see it use CPU as runner
    ```
    msg="starting llama server" cmd="/tmp/ollama3547827739/runners/cpu_avx2/ollama_llama_server ...
    ```
    This patch fix this, and also give a warning if use cpu runner
    instead of GPU.
    
    Signed-off-by: Lei Jitang <leijitang@outlook.com>

--- llm/server.go
+++ llm/server.go
@@ -110,6 +110,7 @@ func NewLlamaServer(gpus gpu.GpuInfoList, model string, ggml *GGML, adapters, pr
 			opts.NumGPU = 0
 		case gpus[0].Library != "metal" && layers == 0:
 			// Don't bother loading into the GPU if no layers can fit
+			estimatedVRAM = 0
 			cpuRunner = serverForCpu()
 			gpuCount = 0
 		case opts.NumGPU < 0 && layers > 0 && gpus[0].Library != "cpu":
@@ -130,6 +131,10 @@ func NewLlamaServer(gpus gpu.GpuInfoList, model string, ggml *GGML, adapters, pr
 		servers = []string{cpuRunner}
 	} else {
 		servers = serversForGpu(gpus[0]) // All GPUs in the list are matching Library and Variant
+		if strings.Split(servers[0], "_")[0] == "cpu" {
+			estimatedVRAM = 0
+			slog.Warn("CPU runner selected instead of GPU")
+		}
 	}
 	demandLib := envconfig.LLMLibrary
 	if demandLib != "" {
