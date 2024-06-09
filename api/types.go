9dc79e7ba634f7b606b49ddfe8dbfc2335234943
Richard Lyons <frob@cloudstaff.com>
2024-06-09 04:15:18 +0200

    Add KV overrides to Modefile PARAMETER.

--- api/types.go
+++ api/types.go
@@ -172,6 +172,7 @@ type Runner struct {
 	UseMMap   bool `json:"use_mmap,omitempty"`
 	UseMLock  bool `json:"use_mlock,omitempty"`
 	NumThread int  `json:"num_thread,omitempty"`
+	OverrideKV []string `json:"override_kv,omitempty"`
 }
 
 // EmbeddingRequest is the request passed to [Client.Embeddings].
